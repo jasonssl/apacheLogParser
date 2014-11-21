3ef6d073ec6777607ab35e162b7345b4504f0450
Jason Lim <jason.sslim@gmail.com>
2014-11-21 16:00:48 +0800

    v1.1

--- apacheLogParser.sh
+++ apacheLogParser.sh
@@ -16,12 +16,18 @@ header() {
         echo '        "                      ""'
         echo '                                                                  @jasonsslim'
         echo ' Parser to extract the country origin of site visitor'
+        echo ''
 }
 
 parseSite(){
-        for ip in `cat /var/log/apache2/access.log | grep -oE "^\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | sort | uniq`;do
-                echo -ne "IP: ${red}$ip${nocolor} >> "
-                echo -n `whois $ip | grep -iE ^country: | uniq | tr -d " \t" | cut -d":" -f2`' '
+        for line in `cat /var/log/apache2/access.log | grep -oE "^\b([0-9]{1,3}\.){3}[0-9]{1,3}.*\[(0[1-9]|[12][0-9]|3[01])\/.*\/(20[0-9][0-9]).*]" | sed 's/:.*[-+]0[0-9]00//g' | sed 's/ - - /-/g' | uniq`;do
+                for date in `echo $line | cut -d"-" -f2`;do
+                        echo -ne "${red}Date:${nocolor} $date - "
+                done
+                for ip in `echo -n $line | cut -d"-" -f1`;do
+                        echo -ne "${red}IP:${nocolor} $ip - "
+                        echo -ne "${red}Country:${nocolor}" `whois $ip | grep -iE ^country: | uniq | tr -d " \t" | cut -d":" -f2`
+                done
                 echo ""
         done
 }
